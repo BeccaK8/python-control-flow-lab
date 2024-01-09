303a96d6c34b46ee3d9614e76c126c1e67ca07a2
Rebecca Kiefer <rebeccakiefer8@gmail.com>
2024-01-09 14:42:26 -0600

    calculate season

--- control_flow_lab.py
+++ control_flow_lab.py
@@ -113,6 +113,7 @@ print(f"A triangle with sides of {a}, {b} & {c} is a {triangle_type} triangle")
 # The next number is found by adding the two numbers before it
 # Use a while loop with a looping variable, or look into Python ranges, e.g.:
 #   for n in range(50):
+'''
 num1 = 0
 num2 = 1
 next_num = num2
@@ -127,6 +128,7 @@ for n in range(2, 50):
     num2 = next_num
     next_num = num1 + num2
 
+'''
 
 
 # exercise-06 What's the Season?
@@ -150,3 +152,25 @@ for n in range(2, 50):
 #
 # After setting the likely season, you can use another if...elif...else statement to "adjust" if
 # the day number falls within a certain range.
+input_month = input("Enter the month of the year (Jan - Dec): ")
+input_day = int(input("Enter the day of the month: "))
+season = ""
+
+if input_month in ("Jan", "Feb", "Mar"):
+    season = "Winter"
+    if input_month == "Mar" and input_day > 19:
+        season = "Spring"
+elif input_month in ("Apr", "May", "Jun"):
+    season = "Spring"
+    if input_month == "Jun" and input_day > 20:
+        season = "Summer"
+elif input_month in ("Jul", "Aug", "Sep"):
+    season = "Summer"
+    if input_month == "Sep" and input_day > 21:
+        season = "Fall"
+else:
+    season = "Fall"
+    if input_month == "Dec" and input_day > 20: 
+        season = "Winter"
+
+print(f"{input_month} {input_day} is in {season}")
